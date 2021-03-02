418cb3c7547e184d137964b7de98d464419d6a98
sunny <ricklee1998@naver.com>
2021-03-02 16:42:01 +0900

    push
    push

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# knowledgetalk_4
+3명 이상일 때 동작하는 소스코드 이므로 
+index.html을 3개 이상 띄우고 test 하시기 바랍니다.
+
+host: CreateRoom -> RoomJoin
+p1: roomId 입력 -> RoomJoin
+p2: roomId 입력 -> RoomJoin
+
+3명 이상 RoomJoin 하게 될 경우 SDP 버튼이 활성화되고 각각의 페이지에서 SDP 버튼을 클릭하면 참가자의 영상이 전송됩니다.
---
+++ index.js
@@ -0,0 +1,283 @@
+//socket 연결
+const clientIo = io.connect("https://dev.knowledgetalk.co.kr:7103/SignalServer",{});
+
+const roomIdInput = document.getElementById("roomIdInput");
+const videoBox = document.getElementById("videoBox");
+const printBox = document.getElementById("printBox")
+
+const CreateRoomBtn = document.getElementById("CreateRoomBtn");
+const RoomJoinBtn = document.getElementById("RoomJoinBtn");
+const SDPBtn = document.getElementById("SDPBtn");
+
+const CPCODE = "KP-CCC-demouser-01"
+const AUTHKEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoidGVzdHNlcnZpY2UiLCJtYXhVc2VyIjoiMTAwIiwic3RhcnREYXRlIjoiMjAyMC0wOC0yMCIsImVuZERhdGUiOiIyMDIwLTEyLTMwIiwiYXV0aENvZGUiOiJLUC1DQ0MtdGVzdHNlcnZpY2UtMDEiLCJjb21wYW55Q29kZSI6IkxJQy0wMyIsImlhdCI6MTU5Nzk3NjQ3Mn0.xh_JgK67rNPufN2WoBa_37LzenuX_P7IEvvx5IbFZI4"
+
+let members;
+let roomId;
+let userId;
+
+let peers = {};
+let streams = {};
+
+/********************** 기타 method **********************/
+
+//print log on page
+const socketLog = (type, contents) => {
+    let jsonContents = JSON.stringify(contents);
+    const textLine = document.createElement("p");
+    const textContents = document.createTextNode(`[${type}] ${jsonContents}`);
+    textLine.appendChild(textContents);
+    printBox.appendChild(textLine);
+}
+
+//send message to signaling server
+const sendData = data => {
+    data.cpCode = CPCODE
+    data.authKey = AUTHKEY
+    socketLog('send', data);
+    clientIo.emit("knowledgetalk", data);
+}
+
+const deletePeers = async () => {
+    for(let key in streams) {
+        if (streams[key] && streams[key].getTracks()) {
+            streams[key].getTracks().forEach(track => {
+                track.stop();
+            })
+
+            document.getElementById(key).srcObject = null;
+            document.getElementById(key).remove();
+        }
+    }
+
+    for(let key in peers) {
+        if (peers[key]) {
+            peers[key].close();
+            peers[key] = null;
+        }
+    }
+}
+
+//영상 출력 화면 Box 생성
+const createVideoBox = id => {
+    let videoContainner = document.createElement("div");
+    videoContainner.classList = "multi-video";
+    videoContainner.id = id;
+
+    let videoLabel = document.createElement("p");
+    let videoLabelText = document.createTextNode(id);
+    videoLabel.appendChild(videoLabelText);
+
+    videoContainner.appendChild(videoLabel);
+
+    let multiVideo = document.createElement("video");
+    multiVideo.autoplay = true;
+    multiVideo.id = "multiVideo-" + id;
+    videoContainner.appendChild(multiVideo);
+
+    videoBox.appendChild(videoContainner);
+}
+
+//Local stream, peer 생성 및 sdp return
+const createSDPOffer = async id => {
+    return new Promise(async (resolve, reject) => {
+        peers[id] = new RTCPeerConnection();
+        streams[id] = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+        let str = 'multiVideo-'+id;
+        let multiVideo = document.getElementById(str);
+        multiVideo.srcObject = streams[id];
+        streams[id].getTracks().forEach(track => {
+            peers[id].addTrack(track, streams[id]);
+        });
+
+        peers[id].createOffer().then(sdp => {
+            peers[id].setLocalDescription(sdp);
+            return sdp;
+        }).then(sdp => {
+            resolve(sdp);
+        })
+    })
+}
+
+//send sdp answer
+const createSDPAnswer = async data => {
+    let displayId = data.displayId;
+
+    peers[displayId] = new RTCPeerConnection();
+    peers[displayId].ontrack = e => {
+        streams[displayId] = e.streams[0];
+
+        let multiVideo = document.getElementById(`multiVideo-${displayId}`);
+        multiVideo.srcObject = streams[displayId];
+    }
+
+    await peers[displayId].setRemoteDescription(data.sdp);
+    let answerSdp = await peers[displayId].createAnswer();
+    await peers[displayId].setLocalDescription(answerSdp);
+    peers[displayId].onicecandidate = e => {
+        if(!e.candidate){
+            let reqData = {
+                "eventOp": "SDP",
+                "sdp": peers[displayId].localDescription,
+                "roomId": data.roomId,
+                "usage": "cam",
+                "pluginId": data.pluginId,
+                "userId": userId
+            };
+
+            sendData(reqData);
+        }
+    }
+}
+
+//퇴장 시, stream,peer 제거
+const leaveParticipant = id => {
+    document.getElementById(`multiVideo-${id}`).remove();
+    document.getElementById(id).remove();
+
+    if(streams[id]){
+        streams[id].getVideoTracks()[0].stop();
+        streams[id].getAudioTracks()[0].stop();
+        streams[id] = null;
+        delete streams[id];
+    }
+
+    if(peers[id]){
+        peers[id].close();
+        peers[id] = null;
+        delete peers[id];
+    }
+
+}
+
+/********************** button event **********************/
+CreateRoomBtn.addEventListener('click', () => {
+    host = true;
+    let data = {
+        "eventOp":"CreateRoom"
+    }
+
+    sendData(data);
+});
+
+RoomJoinBtn.addEventListener('click', () => {
+    let data = {
+        "eventOp":"RoomJoin",
+        "roomId": roomIdInput.value
+    }
+
+    sendData(data);
+});
+
+SDPBtn.addEventListener('click', async () => {
+
+    let sdp = await createSDPOffer(userId);
+
+    let data = {
+        "eventOp":"SDP",
+        "pluginId": undefined,
+        "roomId": roomIdInput.value,
+        "sdp": sdp,
+        "usage": "cam",
+        "userId": userId
+    }
+
+    sendData(data);
+})
+
+
+
+/********************** event receive **********************/
+clientIo.on("knowledgetalk", async data => {
+
+    socketLog('receive', data);
+
+    switch(data.eventOp || data.signalOp) {
+        case 'CreateRoom':
+            if(data.code == '200'){
+                createRoom(data);
+                CreateRoomBtn.disabled = true;
+            }
+            break;
+
+        case 'RoomJoin':
+            if(data.code == '200'){
+                roomJoin(data);
+                RoomJoinBtn.disabled = true;
+                CreateRoomBtn.disabled = true;
+            }
+            break;
+
+        case 'StartSession':
+            startSession(data);
+            break;
+
+        case 'SDP':
+            if(data.useMediaSvr == 'Y'){
+                if(data.sdp && data.sdp.type == 'offer'){
+                    createSDPAnswer(data);
+                }
+                else if(data.sdp && data.sdp.type == 'answer'){
+                    peers[userId].setRemoteDescription(new RTCSessionDescription(data.sdp));
+                }
+            }
+            break;
+        case 'ReceiveFeed':
+            receiveFeed(data)
+            break;
+
+        case 'Presence':
+            if(data.action == 'exit'){
+                leaveParticipant(data.userId)
+            }
+            break;
+
+    }
+
+});
+
+
+const createRoom = data => {
+    roomIdInput.value = data.roomId;
+
+    //room id copy to clipboard
+    roomIdInput.select();
+    roomIdInput.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+
+    alert('room id copied')
+}
+
+const roomJoin = data => {
+    userId = data.userId;
+}
+
+const startSession = async data => {
+    members = Object.keys(data.members);
+
+    //3명 이상일 때, 다자간 통화 연결 시작
+    if(data.useMediaSvr == 'Y'){
+        for(let i=0; i<members.length; ++i){
+            let user = document.getElementById(members[i]);
+            if(!user){
+                createVideoBox(members[i]);
+            }
+        }
+
+        SDPBtn.disabled = false;
+    }
+}
+
+const receiveFeed = (data) => {
+    data.feeds.forEach(result => {
+        let data = {
+            "eventOp":"SendFeed",
+            "roomId": roomIdInput.value,
+            "usage": "cam",
+            "feedId": result.id,
+            "display": result.display
+        }
+
+        sendData(data);
+    })
+}
